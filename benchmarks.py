717d208dc784321d00d5b4255fdd0deaf073fc1d
Diego <diego@symbioticeda.com>
2019-10-07 17:45:05 +0000

    Restructuring the benchmarks script, so a "Small|Large" keys can be used to perform a type of benchmark. Added recipe examples to show how JSON keys are used. Modified logic utilisation scripts to gather missing information that is used in new report format, as well as generate a CSV file with such information. Modified makefiles to execute either Small or Large benchmarks based on JSON keys.

--- benchmarks.py
+++ benchmarks.py
@@ -19,50 +19,55 @@ def execute_command(cmd):
     if (process != 0):
         logging.error("An error occurred while running: {}".format(cmd))
 
-def execute_json(data):
+def run_main(runtype, **recipeFile):
+    command=('{} make -j$(nproc) DIRECTIVE={} DIR={}'.format(runtype, recipeFile['Directive'], recipeFile['Report']))
+    if (recipeFile["Tool"] == "vivado"):
+        logging.info("Moving to Vivado directory to store results. Executing command {}".format(command))
+        os.chdir(vivado_makefile)
+        execute_command(command+"&")
+        os.chdir(sys.path[0])
+    elif (recipeFile["Tool"] == "yosys"):
+        command=(command+' YSARGS="{}"'.format(recipeFile["YosysArgs"]))
+        logging.info("Moving to Yosys directory to store results. Executing command {}".format(command))
+        os.chdir(yosys_abc9_makefile)
+        execute_command(command+"&")
+        os.chdir(sys.path[0])
+    elif (recipeFile["Tool"] == "ise"):
+        logging.info("Moving to ISE directory to store results. Executing command {}".format(command))
+        os.chdir(ise_makefile)
+        execute_command(command+"&")
+        os.chdir(sys.path[0])
+    elif (recipeFile["Tool"] == "yosys_s6"):
+        command=(command+' YSARGS="{}"'.format(recipeFile["YosysArgs"]))
+        logging.info("Moving to Yosys S6 directory to store results. Executing command {}".format(command))
+        os.chdir(yosys_s6_makefile)
+        execute_command(command+"&")
+        os.chdir(sys.path[0])
+    else:
+        logging.warning("No process executed. Review the JSON recipe file")
+ 
+def execute_json2(data):
     """
     This function parses a JSON file
-    to get the information of tool for synthesis (yosys, vivado, ise)
+    to get the information of tool for synthesis (yosys, vivado, ise),
     directives and possibly custom flow (TODO)
     """
     with open (data, 'r') as f:
         recipeFile = json.load(f)
+        logging.info("Keys are {}".format(recipeFile.keys()))
 
-        for tool in recipeFile:
-            logging.info("Tool selected for running is: {}".format(tool['Tool']))
-
-        for directive in recipeFile:
-            logging.info("Directive to perform benchmark is: {}".format(directive["Directive"]))
-
-        for items in range(len(recipeFile)):
-            command=('make -j$(nproc) DIRECTIVE={} DIR={}'.format(recipeFile[items]["Directive"], recipeFile[items]["Report"]))
-            if (recipeFile[items]["Tool"] == "vivado"):
-                logging.info("Moving to Vivado directory to store results")
-                os.chdir(vivado_makefile)
-                logging.info("Executing Vivado Makefile")
-                execute_command(command)
-                os.chdir(sys.path[0])
-            elif (recipeFile[items]["Tool"] == "yosys"):
-                command=(command+' YSARGS="{}"&'.format(recipeFile[items]["YosysArgs"]))
-                logging.info("Moving to Yosys directory to store results. Executing command {}".format(command))
-                os.chdir(yosys_abc9_makefile)
-                logging.info("Executing Yosys Makefile")
-                execute_command(command)
-                os.chdir(sys.path[0])
-            elif (recipeFile[items]["Tool"] == "ise"):
-                logging.info("Moving to ISE directory to store results")
-                os.chdir(ise_makefile)
-                logging.info("Executing ISE Makefile")
-                logging.info("Command is {}".format(command))
-                execute_command(command)
-            elif (recipeFile[items]["Tool"] == "yosys_s6"):
-                logging.info("Moving to Yosys S6 directory to store results")
-                os.chdir(yosys_s6_makefile)
-                logging.info("Executing Yosys Makefile")
-                logging.info("Command is {}".format(command))
-                execute_command(command)
+        for key in recipeFile.keys():
+            logging.info("Key is {}".format(key))
+            if key in ('Large', 'Small'):
+                runtype=("{}=1".format(key.upper()))
+                for values in recipeFile[key]:
+                    logging.info("Tool selected for running is: {}".format(values['Tool']))
+                    logging.info("Directive to perform benchmark is: {}".format(values['Directive']))
+                    logging.info("Type of benchmark is: {}".format(key))
+                    run_main(runtype, **values)
             else:
-                logging.warning("No process executed. Review the JSON recipe file")
+                raise Exception('Value selected for benchmark type is wrong: {}. It shoule be either Small or Large. Please review the JSON file'.format(key))
+
 """ 
 Main
 """
@@ -72,5 +77,6 @@ if __name__=="__main__":
                         help="Recipe in JSON format for Benchmarks",
                         type=argparse.FileType('r'))
     arguments=parser.parse_args()
-    execute_json(arguments.recipe[0].name)
+    execute_json2(arguments.recipe[0].name)
+
 
--- scripts/ise_min_period_table.sh
+++ scripts/ise_min_period_table.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for D in *; do
-    if [ -d "$D" ] && [ -f "$D/results.txt" ]; then
+for D in *; do    
+if [ -d "$D" ] && [ -f "$D/results.txt" ]; then
         min_period="$(<$D/results.txt)"
         period="${min_period:0: -1}.${min_period: -1}"
         fmax=`echo "scale=3; (1/$period)*1000" | bc | sed 's!\.0*$!!'`
@@ -10,23 +10,29 @@ for D in *; do
         flops=`sed -n "s/^\s\+Number used as \(Flip Flops\):\s\+\([0-9,]\+\).*/\tRegister as \1: \2/p" "$D/test_$min_period.txt" | sed 's/[^0-9]*//g' | tail -n 1`
         latch=`sed -n "s/^\s\+Number used as \(Latches\):\s\+\([0-9,]\+\).*/\tRegister as \1: \2/p" "$D/test_$min_period.txt" | sed 's/[^0-9]*//g' | tail -n 1`
         muxcy=`sed -n "s/^\s\+\(Number of MUXCYs used:\)\s\+\([0-9,]\+\).*/\tMUXCY: \2/p" "$D/test_$min_period.txt" | sed 's/[^0-9]*//g' | tail -n 1`
-        #f7=`sed -n "s/^#\s\+\(MUXF7\)\s\+:\s\+\([0-9]\+\).*/\2/p" "$D/test_$min_period.txt" | tail -n 1`
-        #f8=`sed -n "s/^#\s\+\(MUXF8\)\s\+:\s\+\([0-9]\+\).*/\2/p" "$D/test_$min_period.txt" | tail -n 1`
-        #echo "	F7 Muxes: ${f7:-0}"
-        #echo "	F8 Muxes: ${f8:-0}"
-        #echo "	Fx Muxes: $((${f7:-0}+${f8:-0}))"
-	#muxfx=$(($f7+$f8))
+        f7=`sed -n "s/^#\s\+\(MUXF7\)\s\+:\s\+\([0-9]\+\).*/\2/p" "$D/test_$min_period.txt" | tail -n 1`
+        f8=`sed -n "s/^#\s\+\(MUXF8\)\s\+:\s\+\([0-9]\+\).*/\2/p" "$D/test_$min_period.txt" | tail -n 1`
+        #echo " F7 Muxes: ${f7:-0}"
+        #echo " F8 Muxes: ${f8:-0}"
+        #echo " Fx Muxes: $((${f7:-0}+${f8:-0}))"
+        muxfx=$((${f7:-0}+${f8:-0}))
         dsp=`sed -n "s/^\s\+Number of \(DSP48A1\)s:\s\+\([0-9]\+\).*/\t\1: \2/p" "$D/test_$min_period.txt" | sed -e 's/.*:[0-9 ]//g' | tail -n 1`
-	if [ -f "$D/yosys.txt" ]; then
+        if [ -f "$D/yosys.txt" ]; then
                 tool=`sed -n 's/.*\(Yosys\ [0-9].[0-9].*\).*/\1/p' $D/yosys.txt | tail -n 1`
+		runtime=`printf "%1.f" $(sed -n 's/.*user\(.*\)system.*/\1/p' "$D/yosys.txt" |  sed -e 's/s\b//g' | tail -n 1)`
+                peakmem=`printf "%1.f" $(sed -n 's/.*total,\(.*\)MB\ resident.*/\1/p' "$D/yosys.txt" |  sed -e 's/s\b//g' | tail -n 1)`
         else
                 tool="ISE 14.7" #TODO: Select appropiate tool/version
-	fi
+		runtime=`printf "%1.f" $(sed -n 's/.*REAL time to NGDBUILD completion:\ \(.*\)sec.*/\1/p' "$D/test_$min_period.txt" |  sed -e 's/s\b//g')`
+		peakmem=`printf "%1.f" $(sed -n 's/.*usage is\(.*\)k.*/\1/p' "$D/test_$min_period.txt" |  sed -e 's/s\b//g' | awk '{$1=$1/1024; print $1;}')`
+        fi
         RAMB16=`sed -n "s/^\s\+\(Number of RAMB16BWERs:\)\s\+\([0-9,]\+\).*/\tRAMB16BWERs: \2/p" "$D/test_$min_period.txt" | sed -e 's/.*:[0-9 ]//g' | tail -n 1`
         RAMB8=`sed -n "s/^\s\+\(Number of RAMB8BWERs:\)\s\+\([0-9,]\+\).*/\tRAMB16BWERs: \2/p" "$D/test_$min_period.txt" | sed -e 's/.*:[0-9 ]//g' | tail -n 1`
-        bram=$(($RAMB16+$RAMB8)) 
-	echo "Design: $D"
-	echo ",Tool,LUT,FF,BRAM,LUTRAM,DSP,SRL,CARRY4,MuxFx,Logic Levels,Max Frequency,Peak Runtime in s,Peak Memory in MB"
-	echo ",\"$tool\",$lut_logic,$flops,$bram,$dram,$dsp,$lut_slr,$muxcy,$fmax"
+        bram=$(($RAMB16+$RAMB8))
+        echo "Design: $D"
+        #echo ",Tool,LUT,FF,BRAM,LUTRAM,DSP,SRL,CARRY4,MuxFx,Logic Levels,Max Frequency,Peak Runtime in s,Peak Memory in MB"
+        echo ",\"$tool\",$lut_logic,$flops,$bram,$dram,$dsp,$lut_slr,$muxcy,$muxfx,$fmax,$runtime,$peakmem"
     fi
 done
+
+
